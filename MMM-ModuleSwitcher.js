05d028738960391157d47eb8c73a35e1ed3f8b8e
Harm Tilburgs <htilburgs@live.com>
2026-01-16 09:57:47 +0100

    Update MMM-ModuleSwitcher.js

--- MMM-ModuleSwitcher.js
+++ MMM-ModuleSwitcher.js
@@ -3,7 +3,8 @@ Module.register("MMM-SpotifySwitcher", {
         moduleA: "MMM-Clock",
         spotifyModule: "MMM-OnSpotify",
         checkInterval: 500,
-        animationSpeed: 500
+        animationSpeed: 500,
+        lockString: "SPOTIFY_ACTIVE"
     },
 
     start() {
@@ -20,23 +21,24 @@ Module.register("MMM-SpotifySwitcher", {
         if (!this.moduleA) {
             this.moduleA = modules.find(m => m.name === this.config.moduleA);
         }
-
         if (!this.spotify) {
             this.spotify = modules.find(m => m.name === this.config.spotifyModule);
         }
-
         if (!this.moduleA || !this.spotify) return;
 
         const isActive = this.spotify.hidden === false;
 
-        // Alleen reageren op verandering
         if (isActive !== this.spotifyActive) {
             this.spotifyActive = isActive;
 
             if (isActive) {
-                this.moduleA.hide(this.config.animationSpeed);
+                this.moduleA.hide(this.config.animationSpeed, {
+                    lockString: this.config.lockString
+                });
             } else {
-                this.moduleA.show(this.config.animationSpeed);
+                this.moduleA.show(this.config.animationSpeed, {
+                    lockString: this.config.lockString
+                });
             }
         }
     }
